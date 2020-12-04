fc2ff1be59d661401966db59ed987ebdbdce0b40
Bo Yuan <by276@cornell.edu>
2020-12-03 20:14:58 -0500

    calculate marginal cost

--- model_code/gaussianGradient.m
+++ model_code/gaussianGradient.m
@@ -1,17 +1,11 @@
-function smoothedGradient = gaussianGradient(segBids, pMin, pMax, h)
+function smoothedGradient = gaussianGradient(segBids, price, h)
 %GaussianCurve Summary of this function goes here
 %   Detailed explanation goes here
 numBids = size(segBids, 1);
-price = [pMin:pMax]';
-gradient = zeros(size(price));
-for i=1:size(price, 1)  
-    sum = 0;
-    for k=1:numBids
-        qk = segBids(k, 1);
-        pk = segBids(k, 2);
-        sum = sum + qk/h * normpdf((price(i) - pk)/h);
-    end
-    gradient(i) = sum;
+smoothedGradient = 0;
+for k=1:numBids
+    qk = segBids(k, 1);
+    pk = segBids(k, 2);
+    smoothedGradient = smoothedGradient + qk/h * normpdf((price - pk)/h);
 end
-smoothedGradient = [gradient, price];
 end
